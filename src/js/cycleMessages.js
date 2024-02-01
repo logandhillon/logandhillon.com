75d13ce3543b639a3afe45ed3efd481525676f40
Logan Dhillon <logandhi64@gmail.com>
2024-02-01 13:37:23 -0500

    Shorten message for smaller screens

--- src/js/cycleMessages.js
+++ src/js/cycleMessages.js
@@ -2,7 +2,7 @@
  * Cycle through messages for the hero's <p> tag
  */
 document.addEventListener('DOMContentLoaded', function () {
-    const messages = ["A full stack developer.", "Can program in over 15 languages.", "Front-end, back-end, UI/UX design, and more."];
+    const messages = ["A full stack developer.", "Can program in over 15 languages.", "Design and develop."];
     const message = document.getElementById('message');
     let currMsgIdx = 0;
 
