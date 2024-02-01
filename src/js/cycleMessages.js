ee2da35ae6f133a5f55e0be06be40e5d9a3ee773
Logan Dhillon <logandhi64@gmail.com>
2024-02-01 13:36:27 -0500

    Make page more mobile friendly

--- src/js/cycleMessages.js
+++ src/js/cycleMessages.js
@@ -2,12 +2,12 @@
  * Cycle through messages for the hero's <p> tag
  */
 document.addEventListener('DOMContentLoaded', function () {
-    const messages = ["A full stack developer who works in a bundle of languages.", "Can program in over 15 languages.", "Front-end, back-end, UI/UX design, and more."];
+    const messages = ["A full stack developer.", "Can program in over 15 languages.", "Front-end, back-end, UI/UX design, and more."];
     const message = document.getElementById('message');
     let currMsgIdx = 0;
 
     function cycleMessages() {
-        message.textContent = messages[currMsgIdx];
+        message.innerHTML = messages[currMsgIdx];
         currMsgIdx = (currMsgIdx + 1) % messages.length;
     }
 
