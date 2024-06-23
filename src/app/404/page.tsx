169a23be1581aa96389abe2079f53e5a52d58f69
Logan Dhillon <logandhi64@gmail.com>
2024-06-23 16:49:04 -0400

    Fix go back button

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,15 +1,18 @@
+"use client"
+
 import Header from "@/components/header";
 
-export default function Home() {return <main>
-    <Header />
+export default function Home() {
+	return <main>
+		<Header />
 
-	<section className="container mx-auto p-5 text-center mt-32 mb-72">
-		<h1 className="text-4xl font-semibold">404 Not Found</h1>
-		<p>The content you're looking for could not be found.</p>
-		<p className="mb-4">Sorry about that.</p>
-		<a href="/" className="text-white bg-purple-600 hover:bg-purple-700 py-2 px-4 rounded-md">Go Home</a>
-		<a href="#" onclick="window.history.back()" className="ml-2 text-gray-700 bg-gray-300 hover:bg-gray-400 py-2 px-4 rounded-md">Go Back</a>
-	</section>
-    
-  </main>;
+		<section className="container mx-auto p-5 text-center mt-32 mb-72">
+			<h1 className="text-4xl font-semibold">404 Not Found</h1>
+			<p>The content you're looking for could not be found.</p>
+			<p className="mb-4">Sorry about that.</p>
+			<a href="/" className="text-white bg-purple-600 hover:bg-purple-700 py-2 px-4 rounded-md">Go Home</a>
+			<a href="#" onClick={() => window.history.back()} className="ml-2 text-gray-700 bg-gray-300 hover:bg-gray-400 py-2 px-4 rounded-md">Go Back</a>
+		</section>
+
+	</main>;
 }
