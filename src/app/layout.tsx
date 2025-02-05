aa54271521f71f101571f7881d02e09d3f99c8a3
Logan Dhillon <logandhi64@gmail.com>
2025-02-05 13:13:37 -0500

    refactor: clean up directories - some small tweaks/fixes as well

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 
 import Footer from "@/components/footer";
 import { HeroUIProvider } from "@heroui/react";
@@ -9,8 +9,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Logan Dhillon",
-  description: "Logan Dhillon's web portfolio"
-}
+  description: "Logan Dhillon's web portfolio",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -20,8 +23,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script async src="https://kit.fontawesome.com/5a740087a2.js" crossOrigin="anonymous"></script>
-        <meta name="google-site-verification" content="wpclubyVAcYUAC40rgkUB5RAncvSwdtZkyOa8bxg2cQ" />
+        <script
+          async
+          src="https://kit.fontawesome.com/5a740087a2.js"
+          crossOrigin="anonymous"
+        ></script>
+        <meta
+          name="google-site-verification"
+          content="wpclubyVAcYUAC40rgkUB5RAncvSwdtZkyOa8bxg2cQ"
+        />
       </head>
       <body className={inter.className}>
         <HeroUIProvider>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,24 +1,33 @@
 export default function Footer() {
-	return <footer className="text-gray-600 body-font bg-neutral-950 mt-32">
-		<div className="text-white body-font container text-center px-5 py-16 mx-auto flex items-center sm:flex-row flex-col space-x-2">
-			<div className="sm:text-left">
-				<h3 className="uppercase text-2xl font-bold py-4">Logan Dhillon</h3>
-				<p>A full stack developer who works in a bundle of languages.</p>
-			</div>
-			<div className="flex-grow"></div>
-			<div className="sm:text-right mt-8 sm:mt-0">
-				<h3 className="uppercase text-xl sm:text-2xl font-bold py-4">Connect With Me</h3>
-				<div className="text-xl space-x-4">
-					<a href="mailto:logandhi64@gmail.com"><i className="fas fa-envelope transition-transform hover:scale-150"></i></a>
-					<a href="https://discord.gg/GsUqxzj"><i className="fab fa-discord transition-transform hover:scale-150"></i></a>
-					<a href="https://github.com/logandhillon/"><i className="fab fa-github transition-transform hover:scale-150"></i></a>
-				</div>
-			</div>
-		</div>
-		<div className="text-center">
-			<div className="container mx-auto p-8 border-t border-gray-700">
-				<p className="text-xs text-neutral-50">© 2024 Logan Dhillon. All rights reserved.</p>
-			</div>
-		</div>
-	</footer>;
+  return (
+    <footer className="text-gray-600 body-font bg-neutral-950 mt-32">
+      <div className="text-white body-font container text-center px-5 py-16 mx-auto flex items-center sm:flex-row flex-col space-x-2">
+        <div className="sm:text-left">
+          <h3 className="uppercase text-2xl font-bold py-4">Logan Dhillon</h3>
+          <p>A full stack developer who works in a bundle of languages.</p>
+        </div>
+        <div className="flex-grow"></div>
+        <div className="sm:text-right mt-8 sm:mt-0">
+          <h3 className="uppercase text-xl sm:text-2xl font-bold py-4">
+            Connect With Me
+          </h3>
+          <div className="text-xl space-x-4">
+            <a href="mailto:logandhi64@gmail.com">
+              <i className="fas fa-envelope transition-transform hover:scale-150"></i>
+            </a>
+            <a href="https://github.com/logandhillon/">
+              <i className="fab fa-github transition-transform hover:scale-150"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="text-center">
+        <div className="container mx-auto p-8 border-t border-gray-700">
+          <p className="text-xs text-neutral-50">
+            © {new Date().getFullYear()} Logan Dhillon. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
