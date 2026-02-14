f0f58bccac9af5c5ff7ed13b642a88515ffa4b14
Logan Dhillon <dev@logandhillon.com>
2026-02-14 10:36:45 -0500

    fix: enable trailing slashes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  reactStrictMode: false,
+    output: "export",
+    images: {unoptimized: true},
+    reactStrictMode: false,
+    trailingSlash: true,
 };
 
 export default nextConfig;
